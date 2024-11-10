1780e356cdec9804406c9748fb88df9fa8323b17
unknown <vitormartinscarvalho@gmail.com>
2024-11-10 00:16:36 -0300

    making a lot of changes

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
 from .models import Room
+from .serializers import CreateRoomSerializer, RoomSerializer
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+    
+    def get(self, request, format=None):
+        # Aqui você pode retornar dados de todas as salas (por exemplo, para exibir uma lista de salas)
+        rooms = Room.objects.all()  # Aqui você pode personalizar a consulta, caso queira filtrar
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        # Se a sessão não existir, cria uma nova sessão
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        # Cria o serializer e valida os dados da requisição
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            guest_can_pause = serializer.validated_data.get('guest_can_pause')
+            votes_to_skip = serializer.validated_data.get('votes_to_skip')
+            host = self.request.session.session_key
+
+            # Verifica se já existe uma sala para o host atual
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                # Cria uma nova sala
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+
+            # Retorna a resposta com os dados da sala
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        
+        # Se o serializer não for válido, retorna um erro
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
