18d218a76f0ae639e44ec2dda31994e48155b295
unknown <vitormartinscarvalho@gmail.com>
2024-11-11 11:59:37 -0300

    new changes

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import Button from "@mui/material/Button";
+import { Grid2 } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+
 
 function CreateRoomPage(props) {
-    return <p>This is the Create Room Page</p>
+    //const defaultVotes = 2;
+
+    return (
+        <Grid2 container spacing={1}>
+            <Grid2 item xs={12} style={{ textAlign: "center" }}>
+                <Typography component="h4" variant="h4">
+                    Create A Room
+                </Typography>
+            </Grid2>
+        </Grid2>
+    );
 }
+
 
 export default CreateRoomPage;
